49cb1a527e454a5039fd0878ad602658d376c4fe
Konvaly <konvaly@gmail.com>
2025-11-09 23:25:08 +0000

    Initial commit

---
+++ example1.test.js
@@ -0,0 +1,10 @@
+function largestNum(num1, num2, num3) {
+  return num3;
+}
+
+console.assert(largestNum(5, 3, 7) === 7, "largest number in third position");
+console.assert(largestNum(9, 2, 10) === 7, "largest number in third position");
+console.assert(
+  largestNum(5.2, 3.8, 17.5) === 7,
+  "largest number in third position"
+);
